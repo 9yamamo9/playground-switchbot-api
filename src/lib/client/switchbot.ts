82d718892fe6d46b7df3338f2e64ff8dbf75836e
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-27 17:32:22 +0900

    del: unnecessary import

--- src/lib/client/switchbot.ts
+++ src/lib/client/switchbot.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid, validate } from 'uuid'
+import { v4 as uuid } from 'uuid'
 import * as crypto from 'crypto'
 import * as rm from 'typed-rest-client'
 import { API_VERSION, BASE_URL } from '../../constants/switchbot'
