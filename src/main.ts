26b2c416116242dc0c33a3ef7bd85f680f61f161
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-29 11:42:10 +0900

    add: set and query a webhook configuration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import Switchbot from './lib/client/switchbot'
 import { container } from 'tsyringe'
 import SwitchBotResource from './lib/client/rest'
 
+const webhookUrl = process.env.WEBHOOK_URL || ''
+
 async function main() {
 	container.register('ISwitchbotResource', {
 		useValue: new SwitchBotResource(API_VERSION)
@@ -20,6 +22,12 @@ async function main() {
 		const deviceStatus = await switchbot.getDeviceStatus(device.deviceId)
 		console.log('deviceStatus:', deviceStatus)
 	}
+
+	// const result = await switchbot.setWebhookConfigure(webhookUrl)
+	// console.log('Set Webhook Configuration Result: ', result)
+
+	const webhookConfiguration = await switchbot.queryWebhookConfigure(webhookUrl)
+	console.log('Webhook Configuration: ', webhookConfiguration)
 }
 
 main()
